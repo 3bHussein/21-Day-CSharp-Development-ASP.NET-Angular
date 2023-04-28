43e7587130b78d14d9131904bd6627e157b94bd3
3bHussein <39864308+3bHussein@users.noreply.github.com>
2023-04-28 19:32:10 +0400

    /

--- Refresh/BackToCS/BackToCS/Program.cs
+++ Refresh/BackToCS/BackToCS/Program.cs
@@ -1,3 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Hussein!");
+Console.ReadLine();
 
+
+
+
+//Day 1
+//class people
+//{
+//    private string name;
+//    private int age;
+
+
+//    private static string add(int x)
+//    {
+//        private int total = 0;
+//    total= 
+
+//        return age+;
+//    }
+//}
+
+//class main
+//{
+//    public  string p1 =new people();
+
+//}
+
+
+
+//old .net
+//namespace project;
+
+//class program
+//{
+//    static void Main(string[] args)
+//    {
+
+//    }
+
+
+
+//    static string add(int f)
+//    {
+
+//        private home1 =new home1();
+//        return null;
+
+//    }
+//}
